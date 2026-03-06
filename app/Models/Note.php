272d899c852623f26da47df65aa1335cc799d6ea
Abdelwahab Saeed Younes <abdelwahabsaeed415@gmail.com>
2026-03-06 07:21:28 +0200

    feat: Implement an accessor for the Note model's `reference_link` to ensure it is a valid URL or null.

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -13,4 +13,17 @@ class Note extends Model
         'content',
         'reference_link',
     ];
+
+    public function getReferenceLinkAttribute($value)
+    {
+        if (!$value) {
+            return null;
+        }
+
+        if (!str_starts_with($value, 'http://') && !str_starts_with($value, 'https://')) {
+            return 'https://' . $value;
+        }
+
+        return $value;
+    }
 }
